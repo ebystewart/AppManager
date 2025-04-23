e92fc7eee3ba596c47f95f7aba5706479820f796
Eby Stewart <ebyystewart@gmail.com>
2025-04-23 21:29:05 +0530

    Initial commit

---
+++ AppManager.c
@@ -0,0 +1,191 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <spawn.h>
+#include "AppManager.h"
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct{
+    char *binName;
+    char *binPath;
+    char *arguments;
+    uint32_t uid;
+    uint32_t gid;
+    uint32_t pid;
+}AppAttributes_t;
+
+static bool_t parse_app(FILE *fd);
+static void print_options(void);
+static void parse_attributes(char *sptr, unsigned int str_len);
+
+int main(int argc, char **argv)
+{
+    FILE *fd;
+    char *filePath = NULL;
+    if((argv[2] != NULL) && (strcmp(argv[1], "-c") == 0U) && (argc == 3U))
+    {
+        filePath = (char *)malloc(100);
+        filePath = argv[2];
+        printf("The configuration file path is %s\n", filePath);
+        fd = fopen(filePath, "r");
+    }
+    else if((strcmp(argv[1], "-h") == 0U) && (argc == 2))
+    {
+        print_options();
+        goto end;
+    }
+    else 
+    {
+        printf("Error: Invalid arguments passed\n");
+        print_options();
+        goto end;
+    }
+    if(!fd){
+        free(filePath);
+        goto end;
+    }
+    else{
+        parse_app(fd);
+        fclose (fd);
+    }
+    /* This is to support one entry one exit */   
+    end:
+        exit(0);   
+    return 0;
+}
+
+static void print_options(void)
+{
+    printf("******************************************************************************\n");
+    printf("*********************************** APP Manager ******************************\n");
+    printf("******************************************************************************\n");
+    printf("\t -c <configuration file> : configuartion file containing application\n");
+    printf("\t -h                      : help \n");
+    printf("******************************************************************************\n");
+}
+
+/*  This function gets the number of applications configured in the configuration file
+    Each configuration should start with a '#' symbol.
+    The Start and end of the file is marked by a '$' symbol, without which the configuration file is termed invalid. */
+static unsigned int get_numAppEntries(FILE *fd)
+{
+
+}
+static char *get_executableFile(FILE *fd, unsigned int appNum)
+{
+
+}
+
+static char *get_executableArgs(FILE *fd, unsigned int appNum)
+{
+
+}
+
+static char *get_executableUserId(FILE *fd, unsigned int appNum)
+{
+
+}
+
+static char *get_executableGroupId(FILE *fd, unsigned int appNum)
+{
+
+}
+
+static bool_t parse_app(FILE *fd)
+{
+    char cptr;
+    //char *fptr;
+    char *sptr;
+    /* temporary allocate 1KB in heap to read and store the conf file */
+    char *tmpBuf = (char *)calloc(1, 100);
+
+    cptr = fgetc(fd);
+    if(cptr == '$')
+    {
+        while (cptr != EOF)
+        {
+            cptr = fgetc(fd);
+            if(cptr == '#')
+            {
+                sptr = fgets(tmpBuf, 100, fd);
+                printf("%s\n",tmpBuf);
+                parse_attributes(sptr, strlen(sptr));
+                memset(sptr, '\n', 100);
+            }
+        }
+    }
+    //fgets(tmpBuf, 1000, fd);
+    //printf("%s\n",tmpBuf);
+
+    //fptr = strchr(tmpBuf, '$');
+    //printf("%s\n",fptr+1);
+
+    /* release the heap */
+    free(tmpBuf);
+
+}
+
+static void parse_attributes(char *sptr, unsigned int str_len)
+{
+    char *cptr;
+    char *tmp = malloc(str_len);
+    AppAttributes_t *attr = calloc(1, 150);
+    strncpy(tmp, sptr, str_len);
+    while(cptr = strtok_r(tmp, " ", &tmp)){
+        printf("%s\n",cptr);
+        //strcpy(attr->binName, cptr);
+        if(*cptr == '-')
+        {
+            printf("- is reached\n");
+            if(cptr[1] == 'd')
+            {
+                printf("-d is reached\n");
+                //memcpy(attr->binPath, strtok_r(cptr, "\"", &cptr), strlen(cptr)-3);
+            }
+            if(cptr[1] == 'a'){
+                printf("-a is reached\n");
+                //memcpy(attr->arguments, (cptr+2), strlen(cptr)-3);
+            }
+            if(cptr[1] == 'u'){
+                //printf("-u is reached\n");
+                attr->uid = atoi(cptr+2);
+                printf("UID: %d\n", attr->uid);
+            }
+            if(cptr[1] == 'g'){
+                //printf("-g is reached\n");
+                attr->gid = atoi(cptr+2);
+                printf("GID: %d\n", attr->gid);
+            }
+        }
+    }
+    /*
+    printf("Bin Name: %s\n",attr->binName);
+    printf("Bin Path: %s\n",attr->binPath);
+    printf("Arguments: %s\n",attr->arguments);
+    printf("UID: %d\n",attr->uid);
+    printf("GID: %d\n",attr->gid);
+    */
+}
+
+static void run_app(AppAttributes_t *appAttr)
+{
+    pid_t pid;
+    char *binFile;
+
+    binFile = appAttr->binName;
+    
+
+    posix_spawnattr_t attr;
+    posix_spawnattr_init(&attr);
+    posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSCHEDULER | POSIX_SPAWN_SETPGROUP | POSIX_SPAWN_SETPGROUP);
+    //posix_spawnattr_setschedpolicy(&attr,);
+    //posix_spawnattr_setpgroup(&attr, );
+    //posix_spawnattr_set
+
+    if(posix_spawnp(&pid, binFile, NULL, &attr, &appAttr->arguments, NULL))
+    {
+        perror("spawn failed");
+        exit(0);
+    }
+}
---
+++ AppManager.h
@@ -0,0 +1,8 @@
+#ifndef APP_MANAGER_H
+#define APP_MANAGER_H
+#include <stdbool.h>
+
+typedef unsigned int uint32_t;
+typedef bool bool_t;
+
+#endif
